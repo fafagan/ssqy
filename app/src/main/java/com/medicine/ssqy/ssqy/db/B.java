64d2ff271990f917831d6739cfc95630ec3af515
sj <453696257@qq.com>
2016-12-19 17:26:04 +0800

    AS1

---
+++ app/src/main/java/com/medicine/ssqy/ssqy/db/B.java
@@ -0,0 +1,8 @@
+package com.medicine.ssqy.ssqy.db;
+
+/**
+ * Created by Administrator on 2016/12/19.
+ */
+public class B {
+}
+
---
+++ app/src/main/java/com/medicine/ssqy/ssqy/db/C.java
@@ -0,0 +1,7 @@
+package com.medicine.ssqy.ssqy.db;
+
+/**
+ * Created by Administrator on 2016/12/19.
+ */
+public class C {
+}
